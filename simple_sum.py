da9d738a06bd3df7649d0d8c15ab3141a803d39d
rob <33564815+writetoleft@users.noreply.github.com>
2020-02-13 19:10:06 -0800

    Fix bug in simple_sum.py
    patchnotes - i fucking did it

--- simple_sum.py
+++ simple_sum.py
@@ -1,8 +1,9 @@
-
+# copyright Robert Hebert  
+# fuck yeaAAAAAAAAAAAAAAAAAAAAAAAAAAA
 # Returns the sum of a and b
 def simple_sum(a: int, b: int) -> int:
-    # Something seems... wrong...
-    return a + a
+    # i did it
+    return a + b
     
 
 print("2 + 3 = {0}".format(simple_sum(2,3)))
